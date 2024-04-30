a2a75fb03059a7d263b3b20959b44c9a478d050f
Mohammad-Hadi-Nastoh <168512290+Mohammad-Hadi-Nastoh@users.noreply.github.com>
2024-04-30 14:19:53 +0430

    first commit

---
+++ abc.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ABC() {
+  return (
+    <div>
+      abc
+    </div>
+  )
+}
+
+export default ABC
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "./component/button/Button"
+
+function App() {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
